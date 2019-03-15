f97c47f1b910071fc15ec8662376cf755c36eb94
selimonat <onatselim@gmail.com>
2019-03-15 21:51:19 +0100

    clipping at 254

--- sequences.py
+++ sequences.py
@@ -5,8 +5,10 @@ def dimmer_noisy(total_sample=100,mean=128,sigma=60):
     #(approximate) mean and standard deviation with sample length of
     #total_sample
     current = 0
+    MAXIMUM_VALUE = 254
     while current < total_sample:
         current += 1 
-        brightness = R.randn(1)*sigma+mean
+        value = R.randn(1)*sigma+mean
+        brightness =  value.clip(max = MAXIMUM_VALUE ) #clip to 254
         yield brightness.astype('uint8').tolist()
         
