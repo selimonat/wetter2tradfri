5a895f38edc02bb2e52d447e6652e2196a68d8f4
selimonat <onatselim@gmail.com>
2019-03-17 10:42:42 +0100

    multiple light support

--- behaviors.py
+++ behaviors.py
@@ -1,21 +1,23 @@
 import time
-SAMPLING_RATE = .5 #seconds
+SAMPLING_RATE = .1 #seconds
 import wetter2tradfri.utils as utils
 import wetter2tradfri.sequences as s
-def randomize_brightness(light_index,duration=100,sigma=60):
+def randomize_brightness(light_index,mean=128,duration=100,sigma=60):
     #duration in econds
     
     number_of_samples  = duration/SAMPLING_RATE
     #autenticate api
     api = utils.autenticate_api()
     #get all lights and select one or some
-    light = utils.get_light(api,light_index)
+    lights = utils.get_light(api,light_index)
     #get the light sequence
-    brightness = s.dimmer_noisy(total_sample = number_of_samples , sigma=sigma)
+    brightness = s.dimmer_noisy(total_sample = number_of_samples ,mean=mean, sigma=sigma)
+    #brightness = s.dimmer_exponential(total_sample = number_of_samples ,
+    #                                  scale=sigma,mean=mean)
     #set the light brightness
     counter = 0
     for b in brightness:
-        api(light.light_control.set_dimmer(b[0]))
+        for light in lights:
+            api(light.light_control.set_dimmer(b[0]))
         time.sleep(SAMPLING_RATE)
         counter += 1
-        print(counter)
