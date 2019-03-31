73766e15ca04628fb4253ac5387920fd636b01d2
selimonat <onatselim@gmail.com>
2019-03-31 20:44:06 +0200

    first implementation of running moving average

--- behaviors.py
+++ behaviors.py
@@ -2,7 +2,7 @@ import time
 SAMPLING_RATE = .1 #seconds
 import wetter2tradfri.utils as utils
 import wetter2tradfri.sequences as s
-def randomize_brightness(light_index,mean=128,duration=100,sigma=60):
+def randomize_brightness(light_index,smooth_window=20,mean=128,duration=100,sigma=60):
     #duration in econds
     
     number_of_samples  = duration/SAMPLING_RATE
@@ -11,7 +11,8 @@ def randomize_brightness(light_index,mean=128,duration=100,sigma=60):
     #get all lights and select one or some
     lights = utils.get_light(api,light_index)
     #get the light sequence
-    brightness = s.dimmer_noisy(total_sample = number_of_samples ,mean=mean, sigma=sigma)
+    brightness = s.dimmer_noisy(total_sample = number_of_samples ,mean=mean,
+                                sigma=sigma,smooth_window=smooth_window)
     #brightness = s.dimmer_exponential(total_sample = number_of_samples ,
     #                                  scale=sigma,mean=mean)
     #set the light brightness
@@ -21,12 +22,12 @@ def randomize_brightness(light_index,mean=128,duration=100,sigma=60):
             #measure how long an api call lasts in second, and 
             #see how realistical it is to control many lamps and still
             #satisfy a given SAMPLING_RATE
-            api(light.light_control.set_dimmer(b[0]))
+            api(light.light_control.set_dimmer(b))
         #instead of sleeping given time, compute until when to sleep, because
         #api calls take also some time in these type of synchronous calls.
         time.sleep(SAMPLING_RATE)
         counter += 1
-        print(counter)
+        print((counter,b))
 
 def randomize_color(light_index,duration=100,sigma=.02):
     #duration in seconds
--- sequences.py
+++ sequences.py
@@ -1,16 +1,21 @@
-from numpy import random as R
+import numpy as np
 MAXIMUM_VALUE = 254
 from wetter2tradfri import utils as u
 
-def dimmer_noisy(total_sample=100,mean=128,sigma=60):
+def dimmer_noisy(total_sample=100,smooth_window = 1, mean=128,sigma=60):
     #Returns an unsigned 8bit integer generator with a specific 
     #(approximate) mean and standard deviation with sample length of
     #total_sample
+    value   = np.random.randn(smooth_window)
     current = 0
-    while current < total_sample:
+    while current < total_sample-smooth_window+1:
         current += 1 
-        value = R.randn(1)*sigma+mean
-        brightness =  value.clip(max = MAXIMUM_VALUE ) #clip to 254
+        #shift and append a random value
+        last_value = np.random.randn(1)*sigma+mean
+        value = np.append(value[1:],last_value)
+        #average the window
+        output = np.mean(value)
+        brightness =  output.clip(max = MAXIMUM_VALUE ) #clip to 254
         yield brightness.astype('uint8').tolist()
         
 def dimmer_exponential(total_sample=100,mean=128,scale=20):
@@ -18,8 +23,8 @@ def dimmer_exponential(total_sample=100,mean=128,scale=20):
     current = 0
     while current < total_sample:
         current += 1
-        sign  = R.choice([-1, 1],1)
-        value = mean+R.exponential(scale,1)*sign
+        sign  = np.random.choice([-1, 1],1)
+        value = mean+np.random.exponential(scale,1)*sign
         brightness =  value.clip(max = MAXIMUM_VALUE ) #clip to 254
         yield brightness.astype('uint8').tolist()
 
@@ -27,6 +32,6 @@ def xy_randomwalk(total_sample=100,start_hue=0,start_intensity=.4,sigma=.1):
     current = 0
     while current < total_sample:
         current += 1 
-        start_hue = start_hue + R.randn(1)*sigma
+        start_hue = start_hue + np.random.randn(1)*sigma
         yield u.pol2cart(start_hue,start_intensity)
  
