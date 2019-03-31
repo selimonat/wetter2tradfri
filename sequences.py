32ad398d4fbc63c2f69b50babe6c9da16ed978f0
selimonat <onatselim@gmail.com>
2019-03-31 20:18:13 +0200

    exponentially distributed dimmer values and random walk in xy space

--- sequences.py
+++ sequences.py
@@ -1,14 +1,32 @@
 from numpy import random as R
+MAXIMUM_VALUE = 254
+from wetter2tradfri import utils as u
 
 def dimmer_noisy(total_sample=100,mean=128,sigma=60):
     #Returns an unsigned 8bit integer generator with a specific 
     #(approximate) mean and standard deviation with sample length of
     #total_sample
     current = 0
-    MAXIMUM_VALUE = 254
     while current < total_sample:
         current += 1 
         value = R.randn(1)*sigma+mean
         brightness =  value.clip(max = MAXIMUM_VALUE ) #clip to 254
         yield brightness.astype('uint8').tolist()
         
+def dimmer_exponential(total_sample=100,mean=128,scale=20):
+    
+    current = 0
+    while current < total_sample:
+        current += 1
+        sign  = R.choice([-1, 1],1)
+        value = mean+R.exponential(scale,1)*sign
+        brightness =  value.clip(max = MAXIMUM_VALUE ) #clip to 254
+        yield brightness.astype('uint8').tolist()
+
+def xy_randomwalk(total_sample=100,start_hue=0,start_intensity=.4,sigma=.1):
+    current = 0
+    while current < total_sample:
+        current += 1 
+        start_hue = start_hue + R.randn(1)*sigma
+        yield u.pol2cart(start_hue,start_intensity)
+ 
--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 from pytradfri.util import load_json, save_json
-
+import numpy as np
 from pytradfri.api.libcoap_api import APIFactory
 from pytradfri.gateway import Gateway
+
+
 def get_light(api,light_index):
     #returns light(s) selected by light_index 
     gateway = Gateway()
@@ -9,10 +11,10 @@ def get_light(api,light_index):
     devices = api(devices_commands)
     lights = [dev for dev in devices if dev.has_light_control]
     #if many light  wanted 
-    if isinstance(light_index,list):
-        return [lights for l in light_index]
-    #if a single light
-    return lights[light_index]
+    try:
+        return [lights[l] for l in light_index]
+    except:
+        return lights[light_index]
 
 def autenticate_api(host="192.168.2.167"):
     #returns an authenticated API object
@@ -22,3 +24,22 @@ def autenticate_api(host="192.168.2.167"):
     psk = conf[host].get('key')
     api_factory = APIFactory(host=host, psk_id=identity, psk=psk)
     return api_factory.request 
+
+def cart2pol(x, y,white_point=(.3128,.329)):
+    x = x - white_point[0]
+    y = y - white_point[1]
+    rho = np.sqrt(x**2 + y**2)
+    phi = np.arctan2(y, x)
+    print("radius is %f" % rho)
+    print("angle is  %f" % phi)
+    return(rho, phi)
+
+def pol2cart(rho, phi,white_point=(.3128,.329)):
+    x = (rho * np.cos(phi)+white_point[0])
+    x = x.clip(max=65535,min=0)
+    y = (rho * np.sin(phi)+white_point[1])
+    y = y.clip(max=65535,min=0)
+    print(type(x))
+    print("x is %f" % x)
+    print("y is %f" % y)
+    return(int(x*65535), int(y*65535))
