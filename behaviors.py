72a9252c37ed0407c68683b1f93682ea7fe751f4
selimonat <onatselim@gmail.com>
2019-05-19 12:32:32 +0200

    new dimmer program

--- behaviors.py
+++ behaviors.py
@@ -1,7 +1,48 @@
 import time
-SAMPLING_RATE = .1 #seconds
+SAMPLING_RATE = .2 #seconds
 import wetter2tradfri.utils as utils
 import wetter2tradfri.sequences as s
+import datetime
+import numpy as np
+
+def dimmer_program(light_index, start_up,stop_up):
+
+    start_up = datetime.datetime.strptime(start_up,"%H:%M")
+    stop_up  = datetime.datetime.strptime(stop_up,"%H:%M")
+    #transform time to minutes of the day
+    f         = lambda x : x.minute+x.hour*60
+    #minute line of the day
+    time_line = np.linspace(0,24*60-1,24*60,dtype=int)
+
+    #weights for the increment.
+    weights   = np.zeros(24*60,dtype=int)
+    weights[(time_line >= f(start_up)) & (time_line < f(stop_up))] = 1
+
+    #number of brightness steps to cover full range
+    increment = 255/(1+f(stop_up)-f(start_up))
+
+    #get light
+    #autenticate api
+    api       = utils.autenticate_api()
+    #get all lights and select one or some
+    lights    = utils.get_light(api,light_index)
+    print(lights) 
+    #current time
+    now       = datetime.datetime.now() 
+    previous = 0 
+    while f(now) < 24*60:
+        i   = f(now) #current_minute
+        new = max(min(255,previous + weights[i]*increment),0)
+        print("current weight: {}".format(weights[i]))
+        print("increment: {}".format(increment))
+        print("new value: {}".format(new))
+        print("setting new value")
+        for light in lights:
+            api(light.light_control.set_dimmer(new.astype('uint8').tolist()))
+        time.sleep(60)
+        now = datetime.datetime.now()   
+        previous = new
+
 def randomize_brightness(light_index,smooth_window=20,mean=128,duration=100,sigma=60):
     #duration in econds
     
@@ -23,9 +64,9 @@ def randomize_brightness(light_index,smooth_window=20,mean=128,duration=100,sigm
             #see how realistical it is to control many lamps and still
             #satisfy a given SAMPLING_RATE
             api(light.light_control.set_dimmer(b))
-        #instead of sleeping given time, compute until when to sleep, because
-        #api calls take also some time in these type of synchronous calls.
-        time.sleep(SAMPLING_RATE)
+            #instead of sleeping given time, compute until when to sleep, because
+            #api calls take also some time in these type of synchronous calls.
+            time.sleep(SAMPLING_RATE)
         counter += 1
         print((counter,b))
 
